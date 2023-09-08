9b1615125fe486502830e4ad632db23e4c7da144
anrwn <andrenrwn@users.noreply.github.com>
2023-09-09 04:29:54 +0800

    Feature: add <div> time blocks dynamically to DOM. Feature: use epoch time as message key and date/hour comparator to set the past/present/future classes and to set element IDs and hour text strings in each time block. Use toLocaleString to allow future customizations.

--- assets/script.js
+++ assets/script.js
@@ -24,24 +24,59 @@
 */
 
 // Global variables
-
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0
 var yyyy = today.getFullYear();
+var hr = today.getHours();
+var selectedday = today;
+const onehour = 3600000; // 1 hour in epoch milliseconds
+var thishour = Math.floor(Date.parse(selectedday)/onehour)*onehour; // data in hourly blocks
+var show_starthour = thishour - (onehour*11); // 11-hour before
+var show_endhour = thishour + (onehour*12); // 12-hour after
+
+// <!--Example of a time block.The "present" class specifies background color.-->
+var time_block = $($.parseHTML('\
+    <div class= "row time-block" > \
+      <div class="col-2 col-md-1 hour text-center py-3"></div> \
+      <textarea class="col-8 col-md-10 description" rows="3"></textarea> \
+      <button class="btn saveBtn col-2 col-md-1" aria-label="save"> \
+          <i class="fas fa-save" aria-hidden="true"></i> \
+      </button> \
+    </div >\
+    '));
 
 
-// Save button is clicked
+// Build out schedule table for the day
+for (var i = show_starthour; i <= show_endhour; i+=onehour) {
+    let d = new Date(0);
+    d.setUTCSeconds(i/1000);
+    console.log(d, i/1000);
+    let hourstring = d.toLocaleString('en-US', {
+        hour: 'numeric',
+        hour12: true,
+      });
+      
+    time_block.attr("id", "hour-" + hourstring.replace(/ /g,''));
+    if (i < thishour) {
+        time_block.addClass("past");
+    } else if (i === thishour) {
+        time_block.addClass("present");
+    } else {
+        time_block.addClass("future");
+    }
+    time_block.children().eq(0).text(hourstring);
+    time_block.clone().appendTo("#schedulelist");
+}
+
+// Save button is clicked, unblink the save icon
 $("div.row.time-block > .saveBtn").on("click", function () {
     console.log($(this).parent()[0].id);
     $(this).removeClass("blink_me");  // unblink the save button after save
 });
 
+// If content is modified in any textarea section, blink the save icon sibling element
 $("div.row.time-block > textarea").on("input", function () {
     console.log($(this).parent()[0].id);
     $(this).next().addClass("blink_me"); // content changed, apply blink to save button
 });
-
-
-//document.querySelector("#hour-10 > textarea")
-//document.querySelector("body > div > div.row.time-block.present > textarea")
